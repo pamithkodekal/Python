6872cae93f57e07c91f7e5c9ce4f2784c0391605
Pamith Kodekal <pamith.kodekal00@gmail.com>
2026-06-14 17:10:20 +0530

    "Operators"

---
+++ LearningPython/Operators.py
@@ -0,0 +1,25 @@
+#Airthmetic Operators
+
+sum = 3 + 5
+subtraction = 3 - 5
+Multiplication =3 * 5
+division = 5 / 3
+Modulo = 10 %  5 
+
+print(f"sum {sum} \nSubraction {subtraction}\nMultiplication {Multiplication}\nDivision{division}\nModulo{Modulo}")
+
+print("=====================\nLogical Operator+++++++++++++\n")
+#logical Operator
+num1 = 50
+num2 = 60
+
+if num1 == num2 :
+    print("Both are equal")
+if num1 > num2:
+    print(f"Num1 : {num1} is greator than Num2 {num2}")
+else:
+    print(f"Num2 : {num2} is greator than Num1 {num1}")
+if num1 < num2:
+    print(f"Num1 : {num1} is smaller than Num2 {num2}")
+else:
+    print(f"Num2 : {num2} is smaller than Num1 {num1}")
