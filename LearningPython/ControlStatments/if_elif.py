948e1df99572b36c52ed81d34fcd852692526b0b
Pamith Kodekal <pamith.kodekal00@gmail.com>
2026-06-19 20:18:18 +0530

    Control Statments in Python

---
+++ LearningPython/ControlStatments/if_elif.py
@@ -0,0 +1,15 @@
+Baygon_Available = False
+
+Paragon_Available = True
+Hit_Available = True
+
+
+if Baygon_Available :
+    print("USe Baygon to Kill Cockroach")
+elif Paragon_Available:
+    print("Use Paragon to Kill Cockroach")
+elif Hit_Available:
+    print("Use Hit")
+
+else :
+    print("Bhagooo.....")
